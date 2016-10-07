bba1d49e37d62c489e21d345ee86d099f87e382e
Tyler Brown <tylerbro54@yahoo.com>
2016-10-06 19:12:55 -0500

    fixed issue with getInformationDirectlyFromUser() where it would end up in endless loop

--- src/main/java/edu/jsu/mcis/Leaderboard.java
+++ src/main/java/edu/jsu/mcis/Leaderboard.java
@@ -2,10 +2,14 @@
 import java.util.Scanner;
 
 public class Leaderboard {
+	static dataSystem dataBank;
+	
+	Leaderboard(){
+		dataBank = new dataSystem();
+	}
 	
 	public static void main(String[] args) {
 		
-		dataSystem dataBank = new dataSystem();
 		if (args.length == 2){
 			String firstValue = args[0];
 			String secondValue = args[1];
@@ -25,15 +29,26 @@ public class Leaderboard {
 		while(terminate == false){
 			System.out.print("\n\n\n\nHINT: VALID INPUTS ARE student or course\nWould you like to look up student or course info: ");
 			command1 = inputField1.nextLine();
+			System.out.println(command1);
 			System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n");
 			System.out.println("VALID INPUT ARE ONLY INTERGERS\nPlease enter in the id: ");
 			command2 = inputField2.nextLine();
+			System.out.println(terminate);
+			if((command1.equals("student")) || (command1.equals("course"))){
+				terminate = true;
+				System.out.println(terminate);
+				System.out.println("yay");
+			}
+			System.out.println(terminate);
 			try{
-				int.parseInt(command2);
+				Integer.parseInt(command2);
+				System.out.println(command1);
 			}
 			catch(NumberFormatException nfe){
-				
+				terminate = false;
+				System.out.println("error");
 			}
 		}
+	dataBank.checkIfCourseOrStudentIsNeeded();
 	}
 }
