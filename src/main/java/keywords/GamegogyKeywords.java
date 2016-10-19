c10820020ac03cf49467df723b041ae36a81c5ec
nikscot <nscott@stu.jsu.edu>
2016-10-19 17:39:26 -0500

    added some keywords

--- src/main/java/keywords/GamegogyKeywords.java
+++ src/main/java/keywords/GamegogyKeywords.java
@@ -1,7 +1,32 @@
 package keywords;
+import team6.*;
+import java.io.IOException;
 
-//import whatever the filepath is
 
 public class GamegogyKeywords{
+	private Student student;
+	private Course course;
+	
+	
+	public GamegogyKeywords() throws IOException{
+		student = new Student();
+		course = new Course();
+		
+	}
+	
+	public String loadStudentData(){
+		return student.getStudentBasedOnID("111128");
+	}
+	
+	public String loadCourseData(){
+		return course.getCourseBasedOnID("99018");
+	}
+	
+	public String noInfoGiven(){
+		return "";
+	}
+	
+	
+	
 	
 }
