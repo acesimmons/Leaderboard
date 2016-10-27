8a6bae50c0208133bb3ecf2167fc29dd420f047c
Tyler Brown <tylerbro54@yahoo.com>
2016-10-27 18:22:50 -0500

    wrote keyword file for feature two

---
+++ src/main/java/keywords/Feature2Keyword.java
@@ -0,0 +1,27 @@
+package keywords;
+import team6.*;
+import java.io.IOException;
+
+
+public class Feature2Keyword{
+	private String output;
+	
+	public Feature2Keyword(){
+		output = "";
+	}
+	public void startGamegogyCLIWithArguments(String type){
+		GamegogyDatabase database = new GamegogyDatabase();
+		if(type.equals("studentids")){
+			output = database.getStudentIDs();
+		}
+		if(type.equals("courseids")){
+			output = database.getCourseIds();
+		}
+	}
+		
+	public String getCommandLineOutput(){
+		return output;
+	}
+	
+
+}
