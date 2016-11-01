904bed405d027b9895376161827feb9f06ee6eda
Tyler Brown <tylerbro54@yahoo.com>
2016-11-01 18:48:55 -0500

    reworked main and developed new class

---
+++ src/main/java/team6/Controller.java
@@ -0,0 +1,27 @@
+package team6;
+import java.io.IOException;
+
+public class Controller {
+	
+	public String runConsoleCommands(String command, String id){
+		String output = "";
+		GamegogyDatabase database = new GamegogyDatabase();
+		if(command.equals("student")) {
+			output = database.getStudent(id).toString();
+		}
+		
+		else if(command.equals("course")) {
+			output = database.getCourse(id).toString();
+		}
+		
+		else if(command.equals("studentids")) {
+			output = database.getStudentIDs();
+		}
+		
+		else if(command.equals("courseids")) {
+			output = database.getCourseIDs();
+		}
+		
+		return output;
+	}
+}
--- src/main/java/team6/Gamegogy.java
+++ src/main/java/team6/Gamegogy.java
@@ -7,36 +7,15 @@ public class Gamegogy {
 	}
 	
 	public static void main(String[] args) {
+		Controller controller = new Controller();
 		String output = "";
 		
 		if(args.length == 2) {
-			output = runCommand(args[0], args[1]);
+			output = controller.runConsoleCommands(args[0], args[1]);
 		}
 		else if(args.length == 1) {
-			output = runCommand(args[0], "");
+			output = controller.runConsoleCommands(args[0], "");
 		}
 		System.out.println(output);
-	}
-	
-	private static String runCommand(String command, String id){
-		String output = "";
-		GamegogyDatabase database = new GamegogyDatabase();
-		if(command.equals("student")) {
-			output = database.getStudent(id).toString();
-		}
-		
-		else if(command.equals("course")) {
-			output = database.getCourse(id).toString();
-		}
-		
-		else if(command.equals("studentids")) {
-			output = database.getStudentIDs();
-		}
-		
-		else if(command.equals("courseids")) {
-			output = database.getCourseIDs();
-		}
-		
-		return output;
 	}
 }
